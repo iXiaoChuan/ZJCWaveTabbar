c1a94487507b0dbc1cd01797835fddaa5a3f40a5
iXiaoChuan <xiaochuan171090331@outlook.com>
2017-04-11 17:26:32 +0800

    创建本地维护仓库,提交到远端
    Signed-off-by: iXiaoChuan <xiaochuan171090331@outlook.com>

---
+++ Example/Pods/Target Support Files/Pods-ZJCWaveTabbar_Tests/Pods-ZJCWaveTabbar_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZJCWaveTabbar_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZJCWaveTabbar_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZJCWaveTabbar/ZJCWaveTabbar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZJCWaveTabbar : NSObject
+@end
+@implementation PodsDummy_ZJCWaveTabbar
+@end
